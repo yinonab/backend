2294577ffd5e971d6d2a3c1dc8b5fa1e4a048c26
yinonab <yinonabarjel@gmail.com>
2023-10-01 16:03:02 +0300

    /M/convert-mongo atlas

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,6 @@
 export default {
-    dbURL: 'mongodb://127.0.0.1:27017',
+    // dbURL: 'mongodb://127.0.0.1:27017',
+    dbURL: 'mongodb+srv://yinonabarjel:Wishime1@cluster0.pn2bkb6.mongodb.net/?retryWrites=true&w=majority',
     dbName: 'toyDB',
     // dbName: 'userDB',
 }
