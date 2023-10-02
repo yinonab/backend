4874400143f88bc231b03f8e015d7796919b5c7a
yinonab <yinonabarjel@gmail.com>
2023-10-02 20:18:31 +0300

    M/add-socket  to backend

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+import http from 'http'
+
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
@@ -11,6 +13,8 @@ import { logger } from './services/logger.service.js'
 logger.info('server.js loaded...')
 
 const app = express()
+const server = http.createServer(app)
+
 
 // Express App Config
 app.use(cookieParser())
@@ -42,6 +46,8 @@ import { authRoutes } from './api/auth/auth.routes.js'
 import { userRoutes } from './api/user/user.routes.js'
 import { toyRoutes } from './api/toy/toy.routes.js'
 import { reviewRoutes } from './api/review/review.routes.js'
+import { setupSocketAPI } from './services/socket.service.js'
+
 
 
 // routes
@@ -52,6 +58,8 @@ app.use('/api/auth', authRoutes)
 app.use('/api/user', userRoutes)
 app.use('/api/toy', toyRoutes)
 app.use('/api/review', reviewRoutes)
+setupSocketAPI(server)
+
 
 // Make every unmatched server-side-route fall back to index.html
 // So when requesting http://localhost:3030/index.html/toy/123 it will still respond with
@@ -63,6 +71,6 @@ app.get('/**', (req, res) => {
 
 const port = process.env.PORT || 3030
 
-app.listen(port, () => {
+server.listen(port, () => {
     logger.info('Server is running on port: ' + port)
 })
